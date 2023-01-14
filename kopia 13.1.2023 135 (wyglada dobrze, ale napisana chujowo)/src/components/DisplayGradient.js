351603a19ef0f1ea12bab20bb7ff11216f8291e4
M1chae1M <michalmaru10@gmail.com>
2023-01-14 02:06:56 +0100

    first upload

--- kopia 13.1.2023 135 (wyglada dobrze, ale napisana chujowo)/src/components/DisplayGradient.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-class DisplayGradient extends React.Component{
-render(){
-const {displayedGradient}=this.props;
-return(<div id='DisplayGradient' Style={displayedGradient}></div>);
-}}
-export default DisplayGradient;
--- kopia 13.1.2023 135 (wyglada dobrze, ale napisana chujowo)/src/components/classWithObjects/NewColor.js
+++
@@ -1,2 +0,0 @@
-class NewColor{constructor(colorID, color, startsOnPercent){this.colorID=colorID;this.color=color;this.startsOnPercent=startsOnPercent;}}
-export default NewColor;
